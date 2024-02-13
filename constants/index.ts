b383a407cd23d19eeb59736a75dc9d4eb89544ca
psypher1337 <vlad.dragusanu@gmail.com>
2024-02-13 11:39:26 +0200

    added left sidebar

---
+++ constants/index.ts
@@ -0,0 +1,80 @@
+export const sidebarLinks = [
+  {
+    id: "sidebar_home",
+    imgURL: "/assets/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    id: "sidebar_search",
+    imgURL: "/assets/search.svg",
+    route: "/search",
+    label: "Search",
+  },
+  {
+    id: "sidebar_activity",
+    imgURL: "/assets/heart.svg",
+    route: "/activity",
+    label: "Activity",
+  },
+  {
+    id: "sidebar_create_thread",
+    imgURL: "/assets/create.svg",
+    route: "/create-thread",
+    label: "Create Thread",
+  },
+  {
+    id: "sidebar_communities",
+    imgURL: "/assets/community.svg",
+    route: "/communities",
+    label: "Communities",
+  },
+  {
+    id: "sidebar_profile",
+    imgURL: "/assets/user.svg",
+    route: "/profile",
+    label: "Profile",
+  },
+];
+
+export const profileTabs = [
+  {
+    id: "profile_threads",
+    value: "threads",
+    label: "Threads",
+    icon: "/assets/reply.svg",
+  },
+  {
+    id: "profile_replies",
+    value: "replies",
+    label: "Replies",
+    icon: "/assets/members.svg",
+  },
+  {
+    id: "profile_tagged",
+    value: "tagged",
+    label: "Tagged",
+    icon: "/assets/tag.svg",
+  },
+];
+
+export const communityTabs = [
+  {
+    id: "community_threads",
+    value: "threads",
+    label: "Threads",
+    icon: "/assets/reply.svg",
+  },
+  {
+    id: "community_members",
+    value: "members",
+    label: "Members",
+    icon: "/assets/members.svg",
+  },
+  {
+    id: "community_requests",
+    value: "requests",
+    label: "Requests",
+    icon: "/assets/request.svg",
+  },
+];
